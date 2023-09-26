1090296afc48d1b899a624761985b91e86454009
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-09-26 10:25:18 -0400

    modified semantic styles

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,13 @@
-
+import { Button } from "semantic-ui-react"
 
 const index = () => {
   return (
     <>
       <div>
         <h2>Games shop</h2>
+        <Button primary>
+          Login
+        </Button>
       </div>
     </>
   )
