1570681d73d9f52949acc300745a5b558407013f
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-09-29 16:35:32 -0400

    (F) frontend: created login form

--- client/src/pages/join/sign-in/sign-in.js
+++ client/src/pages/join/sign-in/sign-in.js
@@ -1,4 +1,6 @@
 import styles from "./sign-in.module.scss"
+import Link from "next/link";
+import { LoginForm } from "@/components/Auth";
 import { JoinLayout } from "@/layouts";
 function SignInPage() {
   return (
@@ -6,6 +8,15 @@ function SignInPage() {
     <JoinLayout>
       <div className={styles.signIn}>
         <h3>Sign in</h3>
+
+
+        <LoginForm />
+
+        <div className={styles.actions}>
+          <Link href="/join/sign-up">
+             Don't have an account? Sign up!
+          </Link>
+        </div>
       </div>
       </JoinLayout>
     </>
