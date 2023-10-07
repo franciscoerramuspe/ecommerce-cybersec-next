51944c15ad2be468061012696824b7be38c3cec5
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-10-07 09:51:46 -0400

    (F) frontend: added logout logic

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,12 @@
 import { Button } from "semantic-ui-react"
+import { useAuth } from "@/hooks"
+
+
+
 
 const index = () => {
+
+  const { user, logout } = useAuth();
   return (
     <>
       <div>
@@ -8,6 +14,19 @@ const index = () => {
         <Button primary>
           Login
         </Button>
+
+        {user ? (
+          <div>
+            <p>Hi, {user.firstname}</p>
+            <Button onClick={logout}>Log out</Button>
+          </div>
+        ) : (
+          <div>
+            <a href="/join/sign-in">
+              Log in
+            </a>
+          </div>
+        )}
       </div>
     </>
   )
