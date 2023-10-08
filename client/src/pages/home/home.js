814864f1ecced0008357043b5bcd1dc8e1d16b3c
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-10-07 21:31:06 -0400

    (f) frontend: created BasicLayout

---
+++ client/src/pages/home/home.js
@@ -0,0 +1,12 @@
+import { BasicLayout } from "@/layouts"
+
+export default function HomePage() {
+  return (
+    <BasicLayout>
+        <h1>
+            HomePage
+        </h1>
+    </BasicLayout>
+  )
+}
+
