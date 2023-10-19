4e3b809cacd4a860f9dc464ae9858dfcfb99cd69
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2023-10-19 16:57:55 -0400

    (F) frontend: added game data to home page

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ export * from "./token";
 export * from "./user";
 export * from "./platform";
 export * from "./address";
-// export * from "./game";
+export * from "./game";
 // export * from "./wishlist";
 // export * from "./cart";
 // export * from "./order";
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,12 +1,15 @@
 import { BasicLayout } from "@/layouts"
+import { Home } from "@/components/Home"
+import { Separator } from "@/components/Shared"
 
 export default function HomePage() {
   return (
+    <>
     <BasicLayout>
-        <h1 style={ { paddingTop: 100 }}>
-            HomePage
-        </h1>
+        <Home.BannerLastGamePublished />
+        <Separator height={100} />
     </BasicLayout>
+    </>
   )
 }
 
